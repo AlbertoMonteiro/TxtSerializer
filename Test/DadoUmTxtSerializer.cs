efe5e2caa138b11d7e95c35b5cff8ee930d4d7f8
Alberto Monteiro <betimbrasil@gmail.com>
2012-08-15 13:57:13 -0300

    First commit

---
+++ Test/DadoUmTxtSerializer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TxtFileGenerator;
+
+namespace Test
+{
+    [TestClass]
+    public class DadoUmTxtSerializer
+    {
+        [TestMethod]
+        public void SerializaçãoDeObjetosDeveSerSeparadoPorPipe()
+        {
+            var txtSerializer = new TxtSerializer();
+
+            var pessoa = new
+            {
+                Id = 1,
+                Nome = "Alberto",
+                Idade = 22
+            };
+
+            var textSerialized = txtSerializer.Serialize(pessoa);
+            Assert.IsTrue(textSerialized.Contains("|1|Alberto|22|"));
+        }
+
+        [TestMethod]
+        public void SerializaçãoDeObjetosDeveConsiderarPropriedadesComplexasNaLinhaAbaixo()
+        {
+            var txtSerializer = new TxtSerializer();
+
+            var pessoa = new
+            {
+                Id = 1,
+                Nome = "Alberto",
+                Idade = 22,
+                Casas = new List<object>
+                {
+                    new { Id=1,Tamanho = 10.9},
+                    new { Id=2,Tamanho = 11.9}
+                }
+            };
+
+            var textSerialized = txtSerializer.Serialize(pessoa);
+
+            const string expected = 
+@"|1|Alberto|22|
+|1|10,9|
+|2|11,9|";
+
+            Assert.IsTrue(textSerialized.Contains(expected));
+        }
+
+        [TestMethod]
+        public void SerializaçãoDeObjetosDeveInserirOIndicadorDeRegistroAPartirDoAtributoTxtRegisterNameCasoHouver()
+        {
+            var txtSerializer = new TxtSerializer();
+
+            var pessoa = new Pessoa
+            {
+                Id = 1,
+                Nome = "Alberto",
+                Pessoas = new List<Pessoa2>
+                {
+                    new Pessoa2 {Id = 2, Nome = "Alberto1"},
+                    new Pessoa2 {Id = 3, Nome = "Alberto2"},
+                }
+            };
+            
+            const string expected =
+@"|I10|1|Alberto|
+|I20|2|Alberto1|
+|I20|3|Alberto2|";
+
+            var textSerialized = txtSerializer.Serialize(pessoa);
+            Assert.IsTrue(textSerialized.Contains(expected));
+        }
+
+        [TxtRegisterName("I10")]
+        class Pessoa
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public List<Pessoa2> Pessoas { get; set; }
+        }
+
+        [TxtRegisterName("I20")]
+        class Pessoa2
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+        }
+    }
+}
---
+++ TxtFileGenerator/TxtSerializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TxtFileGenerator
+{
+    public class TxtSerializer : ITxtSerializer
+    {
+        public string Serialize(object obj)
+        {
+            var sb = new StringBuilder();
+            var type = obj.GetType();
+
+            var txtRegisterName = type.GetCustomAttributes(typeof(TxtRegisterName), false).Cast<TxtRegisterName>().SingleOrDefault();
+
+            var properties = type.GetProperties();
+
+            var values = new List<string>();
+
+            if (txtRegisterName != null)
+                values.Add(txtRegisterName.Name);
+
+            foreach (var property in properties)
+            {
+                var propertyType = property.PropertyType;
+
+                var @interface = propertyType.GetInterface("ienumerable", true);
+
+                if (propertyType.IsPrimitive || typeof(string).IsAssignableFrom(propertyType))
+                    values.Add(property.GetValue(obj, null).ToString());
+                else if (@interface != null)
+                {
+                    var enumerable = property.GetValue(obj, null) as IEnumerable<object>;
+                    if (enumerable != null)
+                    {
+                        foreach (var o in enumerable)
+                            sb.Append(Environment.NewLine + Serialize(o));
+                    }
+                }
+            }
+
+            return string.Format("|{0}|", string.Join("|", values)) + sb;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TxtRegisterName : Attribute
+    {
+        public string Name { get; set; }
+
+        public TxtRegisterName(string name)
+        {
+            Name = name;
+        }
+    }
+}
